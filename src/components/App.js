f9077c703985feb25b1fa429d9d83817e0d6d601
TechStudent11 <razaqhammed36@gmail.com>
2021-12-11 12:23:23 -0500

    make basic app

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.scss"
+
+export default function App(props) {
+    return (
+        <div className="app">
+            <h1>This project was created using <a href="https://github.com/TechStudent11/MiniReactTemplate" target="_blank" rel="noopener noreferrer">MiniReactTemplate</a>. Go star it!</h1>
+        </div>
+    )
+}
